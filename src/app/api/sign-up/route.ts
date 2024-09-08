5dcad91a33375cf18b2c3567e71b628548796f2c
pavanhub-47 <140827035+pavanhub-47@users.noreply.github.com>
2024-09-08 15:24:08 +0530

    sign-up route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -23,11 +23,75 @@ export async function POST(request: Request) {
             );
         }
 
+        const existUserByEmail = await userModel.findOne({ email });
 
-        
+        const verifyCode = Math.floor(
+            100000 + Math.random() * 900000
+        ).toString();
 
+        if (existUserByEmail) {
+            if (existUserByEmail.isVerified) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "username is already with this email",
+                    },
+                    { status: 400 }
+                );
+            } else {
+                const hashPassword = await bcrypt.hash(password, 10);
+                existUserByEmail.password = hashPassword;
+                existUserByEmail.verifyCode = verifyCode;
+                existUserByEmail.verifyCodeExpire = new Date(
+                    Date.now() + 3600000
+                );
+                await existUserByEmail.save();
+            }
+        } else {
+            const hashPassword = await bcrypt.hash(password, 10);
 
+            const expiryDate = new Date();
+            expiryDate.setHours(expiryDate.getHours() + 1);
 
+            const user = await new userModel({
+                username,
+                password: hashPassword,
+                email,
+                verifyCode,
+                verifyCodeExpire: expiryDate,
+                isVerified: false,
+                isAcceptingMessages: true,
+                message: [],
+            });
+
+            await user.save();
+
+            //send verification email
+
+            const emailResponse = await sendVerificationEmail(
+                email,
+                username,
+                verifyCode
+            );
+
+            if (!emailResponse.success) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: emailResponse.message,
+                    },
+                    { status: 500 }
+                );
+            }
+
+            return Response.json(
+                {
+                    success: true,
+                    message: "Email send successfully please verify your email",
+                },
+                { status: 400 }
+            );
+        }
     } catch (error) {
         console.error("Error while registering user", error);
         return Response.json(
